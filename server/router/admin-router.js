b9405e4555d05dc63f1fca1e309333eb8da5060e
dipu-halder <halderdipu076@gmail.com>
2025-06-14 15:40:10 +0530

    fix

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,10 +1,25 @@
-const express = require('express');
+// const express = require('express');
+// const router = express.Router(); 
+// const adminController = require("../controllers/admin-controllers");
+// const authMiddleware = require("../middlewares/auth-middleware")
+// const adminMiddleware =require("../middlewares/admin-middleware")
+
+// router.route('/users').get(authMiddleware,adminMiddleware, adminController.getAllUser)
+// router.route('/users/:id').get(authMiddleware, adminMiddleware, adminController.getUserById);
+// router.route('/users/update/:id').patch(authMiddleware, adminMiddleware, adminController.updateUserId);
+// router.route('/users/delete/:id').delete(authMiddleware, adminMiddleware, adminController.deleteUserBYId);
+
+// router.route('/contacts').get(authMiddleware, adminMiddleware, adminController.getAllcontacts);
+// router.route('/contacts/delete/:id').delete(authMiddleware, adminMiddleware, adminController.deleteContactBYId);
+
+// module.exports = router; 
+  const express = require('express');
 const router = express.Router(); 
 const adminController = require("../controllers/admin-controllers");
 const authMiddleware = require("../middlewares/auth-middleware")
-const adminMiddleware =require("../middlewares/admin-middleware")
+const adminMiddleware = require("../middlewares/admin-middleware");
 
-router.route('/users').get(authMiddleware,adminMiddleware, adminController.getAllUser)
+router.route('/users').get(authMiddleware, adminMiddleware, adminController.getAllUser);
 router.route('/users/:id').get(authMiddleware, adminMiddleware, adminController.getUserById);
 router.route('/users/update/:id').patch(authMiddleware, adminMiddleware, adminController.updateUserId);
 router.route('/users/delete/:id').delete(authMiddleware, adminMiddleware, adminController.deleteUserBYId);
